b31db87f4964bf301f46122c7e3db0fb7339f135
mike5482 <mike5482@gmail.com>
2025-11-02 17:33:31 -0500

    Initial Commit

---
+++ build/CMakeFiles/compile.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compile.dir/scanner.cpp.o"
+  "CMakeFiles/compile.dir/scanner.cpp.o.d"
+  "CMakeFiles/compile.dir/src/listing.cc.o"
+  "CMakeFiles/compile.dir/src/listing.cc.o.d"
+  "compile"
+  "compile.pdb"
+  "scanner.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
